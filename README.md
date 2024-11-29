1a923d6845ac597a680ce37875259444fa2a72b6
Anne Rezendes <annerezendesm@gmail.com>
2024-11-29 02:54:17 -0300

    final commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Learn-Django
 First Django experience with Alura. Creating an API with the framework.
+
+### **Passos para execução**
+1. Clone o repositório:
+   git clone <URL_DO_REPOSITORIO>
+   cd API-Microblog-Django
+
+2. Ative o ambiente virtual
+  .\venv\Scripts\activate 
+
+3. Faça as migrações do bd
+python manage.py migrate
+
+5. Para rodar o teste:
+python manage.py runserver
+
+6. Teste as API's no PostMan! Você pode puxá-las direto do do localhost: http://127.0.0.1:8000/
---
+++ escola/serializer.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from escola.models import Aluno, Curso, Matricula
+
+class AlunoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Aluno
+        fields = '__all__'
+
+class CursoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Curso
+        fields = '__all__'
+
+#serializer faz a ponte da nossa API - filtro de dados que queremos disponibilzar para a API
+
+class MatriculaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Matricula
+        exclude = []
+
+# ...
+
+class ListaMatriculasAlunoSerializer(serializers.ModelSerializer):
+    curso = serializers.ReadOnlyField(source='curso.descricao')
+    periodo = serializers.SerializerMethodField()
+    class Meta:
+        model = Matricula
+        fields = ['curso', 'periodo']
+    def get_periodo(self, obj):
+        return obj.get_periodo_display()
+    
+class ListaAlunosMatriculadosSerializer(serializers.ModelSerializer):
+    aluno_nome = serializers.ReadOnlyField(source = 'aluno.nome')
+    class Meta:
+        model = Matricula
+        fields = ['aluno_nome']
